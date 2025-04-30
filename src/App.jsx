0fbdf70e519bd034d3025bb37d4e99e45fad27ba
Mutuma <ericmutuma15@gmail.com>
2025-04-30 22:09:15 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,221 @@
+import { useState } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import './App.css';
+import './index.css';
+import reactImage from './assets/react.jpeg';
+import flaskImage from './assets/flask.jpeg';
+import nodeImage from './assets/node.jpeg';
+import linuxImage from './assets/linux0.png';
+import powerBIImage from './assets/powerbi1.jpeg';
+import tableauImage from './assets/tableau.png';
+import qgisImage from './assets/qgis.png';
+import arcgisImage from './assets/arcgis0.jpeg';
+import deploymentImage from './assets/deployment.jpeg';
+import versionControlImage from './assets/versioncontrol.png';
+
+import foodcourtImage from './assets/foodcourt.jpeg';
+import desirelinkImage from './assets/desirelink.gif';
+import orionImage from './assets/orion.jpeg';
+import shadowtechImage from './assets/shadowtech.gif';
+import dashboardImage from './assets/dashboard.png';
+import joscarImage from './assets/joscar.gif';
+import mtnImage from './assets/mtn.png';
+import eliteImage from './assets/elite.png';
+import meImage from './assets/me.jpg';
+import contactImage from './assets/contact.jpeg';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={darkMode ? 'dark' : ''} style={{ margin: 0, padding: 0 }}>
+      <div className="font-sans bg-gray-100 dark:bg-gray-900 dark:text-white min-h-screen">
+        {/* Toggle Button */}
+        <div className="p-4 text-right">
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+            <span className="slider"></span>
+          </label>
+        </div>
+
+        {/* Home Section */}
+        <section className="bg-gray-900 text-white py-20 text-center relative">
+          <h1 className="text-4xl font-bold">Hi! I'm a Full-Stack Developer</h1>
+          <p className="mt-4 text-lg">I build smooth, full-stack web apps and create interactive dashboards.</p>
+          <p className="mt-12 mb-8 text-lg">Need a Land surveyor?</p>
+          <div className="mt-4 flex items-center justify-center">
+            <a href="https://orion-geosurveys.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-lg font-bold text-blue-500 hover:text-blue-700 transition-colors flex items-center">
+              Visit Us
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 ml-2 transition-transform transform hover:translate-x-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5l6 6m0 0l-6 6m6-6H3"
+                />
+              </svg>
+            </a>
+          </div>
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+            <div className="animate-bounce">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-8 h-8 mx-auto text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 12.75L12 19.5l-7.5-6.75"
+                />
+              </svg>
+            </div>
+            <p className="mt-2 text-sm">Scroll Down</p>
+          </div>
+        </section>
+
+        <div className="h-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 dark:from-gray-700 dark:via-gray-800 dark:to-gray-700"></div>
+
+        {/* About Section */}
+        <section className="py-20 px-10 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 transition-opacity duration-700" id="about">
+          <h2 className="text-3xl font-bold mb-4">About Me</h2>
+          <div className="flex flex-col md:flex-row items-center gap-6">
+            <img
+              src={meImage}
+              alt="About Me"
+              className="w-48 h-48 rounded-full shadow-lg hover:scale-105 transition-transform duration-300"
+            />
+            <p>
+              I'm a full-stack developer, data analyst, and Linux enthusiast. I work with React (Vite) + Flask or Node.js to build smooth, full-stack web apps. I'm also experienced in Linux system setup (Kali Linux, Ubuntu), handling everything from Wi-Fi and Ethernet configuration to terminal-based troubleshooting.
+            </p>
+          </div>
+        </section>
+
+        <div className="h-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 dark:from-gray-700 dark:via-gray-800 dark:to-gray-700"></div>
+
+        {/* Skills Section */}
+        <section className="py-20 px-10 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 transition-opacity duration-700" id="skills">
+          <h2 className="text-3xl font-bold mb-4">Skills</h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+            {[
+              { name: 'React', image: reactImage },
+              { name: 'Flask', image: flaskImage },
+              { name: 'Node.js', image: nodeImage },
+              { name: 'Linux', image: linuxImage },
+              { name: 'Deployment', image: deploymentImage },
+              { name: 'Version Control', image: versionControlImage },
+              { name: 'Power BI', image: powerBIImage },
+              { name: 'Tableau', image: tableauImage },
+              { name: 'QGIS', image: qgisImage },
+              { name: 'ArcGIS', image: arcgisImage },
+            ].map((skill) => (
+              <div key={skill.name} className="text-center">
+                <img
+                  src={skill.image}
+                  alt={skill.name}
+                  className="mx-auto mb-4 w-32 h-32 object-cover rounded-full shadow-lg hover:scale-110 transition-transform duration-300"
+                />
+                <p className="font-bold text-lg">{skill.name}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <div className="h-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 dark:from-gray-700 dark:via-gray-800 dark:to-gray-700"></div>
+
+        {/* Projects Section */}
+        <section className="py-20 px-10 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 transition-opacity duration-700" id="projects">
+          <h2 className="text-3xl font-bold mb-4">Projects</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {[
+              { name: 'Dashboard', image: dashboardImage, link: 'https://public.tableau.com/app/profile/eric.mutuma/viz/GlobalTBdataAnalysis/GlobalTBprevalenceanalysis?publish=yes' },
+              { name: 'Desire Link', image: desirelinkImage, link: 'https://desirelink.vercel.app' },
+              { name: 'Elite', image: eliteImage, link: 'https://elitemovies15.netlify.app' },
+              { name: 'Food Court', image: foodcourtImage, link: 'https://food-court-g4.onrender.com' },
+              { name: 'Joscar', image: joscarImage, link: 'https://joscargrill.netlify.app' },
+              { name: 'MTN', image: mtnImage, link: 'https://github.com/ericmutuma15/MTN-Cote-D-Ivore-data-analysis/blob/main/Moringa_Data_Science_Prep_Python_Mini_Project_2012_07_Eric_Mutuma_DataReport_.ipynb' },
+              { name: 'Orion', image: orionImage, link: 'https://orion-geosurveys.vercel.app/' },
+              { name: 'Shadow Tech', image: shadowtechImage, link: 'https://shadowtech.netlify.app' },
+            ].map((project) => (
+              <div key={project.name} className="p-6 bg-white dark:bg-gray-700 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className="w-full h-60 object-cover rounded-md mb-4"
+                />
+                <h3 className="font-bold text-xl text-center">{project.name}</h3>
+                <p className="text-sm text-gray-600 dark:text-gray-400 text-center">Description of {project.name}.</p>
+                <div className="text-center mt-4">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+                  >
+                    Visit Site
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <div className="h-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 dark:from-gray-700 dark:via-gray-800 dark:to-gray-700"></div>
+
+        {/* Contact Section */}
+        <section className="py-20 px-10 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 transition-opacity duration-700" id="contact">
+          <h2 className="text-3xl font-bold mb-4">Contact</h2>
+          <div className="flex flex-col md:flex-row items-center gap-6">
+            <img
+              src={contactImage}
+              alt="Contact"
+              className="w-48 h-48 rounded-full shadow-lg hover:scale-105 transition-transform duration-300"
+            />
+            <p>Office Lines:     0100-285-721/ 0716-877 234
+              <br />
+              Mobile: 0798- 244 545
+              <br />
+              Email: ericmutuma15@gmail.com
+            </p>
+            <div className="flex gap-4 mt-4">
+              <a
+                href="mailto:ericmutuma15@gmail.com"
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+              >
+                Mail Me
+              </a>
+              <a
+                href="https://wa.me/254716877234"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition-colors"
+              >
+                WhatsApp
+              </a>
+            </div>
+          </div>
+        </section>
+
+        {/* Footer */}
+        <footer className="bg-gray-900 text-white py-10 text-center">
+          <p>&copy; 2025 Eric Mutuma. All rights reserved.</p>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
